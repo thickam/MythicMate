65d9b1ac2957885e28fab14f7ed8dbc84aa7b626
Tim <9134870+thickam@users.noreply.github.com>
2026-04-23 19:21:18 -0400

    Remove unused dict

--- models/aliased_list.py
+++ models/aliased_list.py
@@ -6,18 +6,6 @@ class AliasedList():
     def get_alias_dict(self) -> dict[str, list[str]]:
         return self.__alias_dict
 
-    dungeon_aliases = {
-    "Pit of Saron":["pos", "pit", "saron"],
-    "Skyreach":["sky", "sr", "s"],
-    "Seat of the Triumvirate":["seat", "sot"],
-    "Algeth'ar Academy":["aa", "algethar", "academy", "algethar academy"],
-    "Magisters' Terrace":["mt", "magisters terrace", "magister", "magisters"],
-    "Maisara Caverns":["mc", "masiara", "cavern", "caverns", "maisara cavern", "trolls"],
-    "Nexus-Point Xenas":["npx", "nexus point", "nexus-point", "xenas", "xexus-noint penas"],
-    "Windrunner Spire":["wrs", "spire", "windrunner", "wind runner", "wind runner spire"],
-    "Any": ["a", "any"]
-    }
-
     def __init__(self, alias_dict: dict[str, list[str]]):
         self.__alias_dict = alias_dict
         self.__build_alias_lookup()
